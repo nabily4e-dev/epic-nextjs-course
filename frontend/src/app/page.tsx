a2b149c3dead2c58ad625aadb2d20054b2c335e6
NabiL Ashbat <nbl.dev21@gmail.com>
2024-07-04 18:05:45 +0300

    part 2 almost done

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,49 @@
-import { Button } from "@/components/ui/button";
+import { HeroSection } from '@/components/custom/HeroSection';
+import qs from 'qs';
 
-export default function Home() {
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      populate: {
+        image: {
+          fields: ['url', 'alternativeText'],
+        },
+        link: {
+          populate: true,
+        },
+      },
+    },
+  },
+});
+
+async function getStrapiData(path: string) {
+  const baseUrl = 'http://127.0.0.1:1337';
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
+  try {
+    const response = await fetch(url.href);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getStrapiData('/api/home-page');
+
+  const { blocks } = strapiData.data.attributes;
+
+  console.dir(blocks, { depth: null });
+
   return (
-    <main className="container mx-auto py-6">
-      <Button>Our Cool Button</Button>
+    <main>
+      <HeroSection data={blocks[0]} />
     </main>
   );
 }
+
+
+
